a6df3e06eb1d2537c323d289bcdc64511dac04bd
maxbilbow <max@rattlemedia.co.uk>
2015-06-14 14:34:26 +0100

    Unity project included

---
+++ UnityProject/Assets/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private Camera[] cameras;
+//	private int cameraNumber = 0;
+//	private int total = Camera.GetAllCameras.count;
+	// Use this for initialization
+	void Start () {
+		this.cameras = new Camera [Camera.allCamerasCount];
+		Camera.GetAllCameras(this.cameras);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (CrossPlatformInputManager.GetButtonUp ("switchCamera")) {
+			bool didEnableCamera = false;
+			bool enableNextCamera = false;
+			while (!didEnableCamera) {
+				for (int i = 0; i < cameras.Length; ++i) {
+					if (enableNextCamera) {
+						cameras[i].enabled = true;
+						enableNextCamera = false;
+						didEnableCamera = true;
+						break;
+					} else if (cameras[i].isActiveAndEnabled) {
+						cameras[i].enabled = false;
+						enableNextCamera = true;
+					}
+				}
+			}
+		}
+	}
+}
