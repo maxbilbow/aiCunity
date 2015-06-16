1a9a49ad746dbb8d1fa631eb632ea2c357ab2364
maxbilbow <max@rattlemedia.co.uk>
2015-06-16 17:02:19 +0100

    Now with jumping.

---
+++ UnityProject/Assets/Max Scripts/RMXBallController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Ball
+{
+	public class RMXBallController : BallUserControl
+	{
+		private bool willJump; // whether the jump button is currently pressed
+		
+
+		public void jump(){
+			#if MOBILE_INPUT
+			willJump = true;
+			CrossPlatformInputManager.SetButtonDown("Jump");
+			print ("Will jump");
+#endif
+		}
+
+#if MOBILE_INPUT
+		private void FixedUpdate()
+		{
+			// Call the Move function of the ball controller
+			//this.ball.Move(move, willJump);
+			if (willJump) {
+				CrossPlatformInputManager.SetButtonUp("Jump");
+				willJump = false;
+			}
+		}
+	
+#endif
+	}
+}
---
+++ UnityProject/Assets/Max Scripts/RMXPlayerActions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RMXPlayerActions : MonoBehaviour {
+
+	UnityStandardAssets.Vehicles.Ball.Ball body;
+	bool willJump = false;
+	// Use this for initialization
+	private void Awake () {
+		body = GameObject.Find (name + "/body").GetComponent<UnityStandardAssets.Vehicles.Ball.Ball> ();
+	}
+
+	public void Jump(){
+		willJump = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (willJump) {
+			body.Move(new Vector3(), willJump);
+			willJump = false;
+		}
+	}
+}
