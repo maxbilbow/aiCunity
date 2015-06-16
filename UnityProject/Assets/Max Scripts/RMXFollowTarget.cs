1f64901fb3bddb4fda1fa5d48f90aef330cb8db9
maxbilbow <max@rattlemedia.co.uk>
2015-06-16 13:43:35 +0100

    Tweaks

---
+++ UnityProject/Assets/Max Scripts/RMXFollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace UnityStandardAssets.Utility
+{
+	public class FollowTarget : MonoBehaviour
+	{
+		public bool negateRotation = true;
+		public Transform target;
+		public Vector3 offset = new Vector3(0f, 7.5f, 0f);
+		
+		
+		private void LateUpdate()
+		{
+			transform.position = target.position + offset;
+//			transform.rot -= target.localRotation;
+		}
+	}
+}
